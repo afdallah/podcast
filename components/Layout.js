21d09a774c598e70bc0787c95bcf63af4acf41a9
afdallah <afdallah.war@gmail.com>
2019-08-14 22:13:10 +0700

    Add simple animation to publish button

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Router from 'next/router'
 import Container from './Container'
 import css from '../styles.scss'
 import { FaGoogle, FaTelegramPlane } from 'react-icons/fa'
@@ -9,76 +10,88 @@ const Player = dynamic(() => import('./Player'), {
   ssr: false
 });
 
-export default ({ children, user, title }) => (
-  <>
-    <Head>
-      <title>{title || 'Marketing podcast'}</title>
-    </Head>
-    <div className={css.layout}>
-      <div className={css.navigation}>
-        <div className={css.navigation__inner}>
-          <ul className={css.menu}>
-            <li className={css.menu__item}>
-              <Link href="/"><a>Home</a></Link>
-            </li>
-            {/* <li className={css.menu__item}><a>Events</a></li>
-            <li className={css.menu__item}><a>Merch</a></li> */}
-          </ul>
+export default ({ children, user, title }) => {
+  let currentPath
+  if (process.browser) {
+    currentPath = Router.router.asPath
+  }
 
-          <ul className={css.menu}>
-            {!user ? (
-              <li className="menu__item">
-                <Link href="/auth/google">
-                  <a
-                    className={[css['button'], css['button-icon'], css['button-md']].join(' ')}
-                  >
-                    <FaGoogle /> Sign In
-                  </a>
-                </Link>
+  return (
+    <>
+      <Head>
+        <title>{title || 'Marketing podcast'}</title>
+      </Head>
+      <div className={css.layout}>
+        <div className={css.navigation}>
+          <div className={css.navigation__inner}>
+            <ul className={css.menu}>
+              <li className={css.menu__item}>
+                <Link href="/"><a>Home</a></Link>
               </li>
-            ) : (
-              <>
-                {(user.level < 2) && (
-                  <li className={css.menu__item}>
-                    <Link href="/publish"><a><FaTelegramPlane />Publish</a></Link>
-                  </li>
-                )}
-                <li className={[css['menu__item'], css['dropdown']].join(' ')}>
-                  <a>
-                    <span className={css.avatar}>
-                      <img src={user.photo.url} alt=""/>
-                    </span>
-                    {user.firstName}
-                  </a>
+              {/* <li className={css.menu__item}><a>Events</a></li>
+              <li className={css.menu__item}><a>Merch</a></li> */}
+            </ul>
 
-                  <ul className={css['dropdown-menu']}>
-                    <li>
-                      <Link href="/profile">
-                        <a>Profile</a>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link href="/logout">
-                        <a>Logout</a>
-                      </Link>
-                    </li>
-                  </ul>
+            <ul className={css.menu}>
+              {!user ? (
+                <li className="menu__item">
+                  <Link href="/auth/google">
+                    <a
+                      className={[css['button'], css['button-icon'], css['button-md']].join(' ')}
+                    >
+                      <FaGoogle /> Sign In
+                    </a>
+                  </Link>
                 </li>
-              </>
-            )}
-          </ul>
+              ) : (
+                <>
+                  <li className={[css['menu__item'], css['dropdown']].join(' ')}>
+                    <a>
+                      <span className={css.avatar}>
+                        <img src={user.photo.url} alt=""/>
+                      </span>
+                      {user.firstName}
+                    </a>
+
+                    <ul className={css['dropdown-menu']}>
+                      <li>
+                        <Link href="/profile">
+                          <a>Profile</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/logout">
+                          <a>Logout</a>
+                        </Link>
+                      </li>
+                    </ul>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </div>
+
+        <Container>
+          {children}
+          {(user && (user.level < 2)) && (currentPath !== '/publish') ? (
+            <Link href="/publish">
+              <a
+                className={[css['button'], css['button--primary'], css['button--float']].join(' ')}
+              >
+                <FaTelegramPlane /> <span>Broadcast now!</span>
+              </a>
+            </Link>
+          ) : ''}
+        </Container>
+        {/* Footer */}
+        <div className={css['fixed-bottom']}>
+          <Container>
+            <Player />
+          </Container>
         </div>
       </div>
+    </>
+  )
+}
 
-      <Container>
-        {children}
-      </Container>
-      {/* Footer */}
-      <div className={css['fixed-bottom']}>
-        <Container>
-          <Player />
-        </Container>
-      </div>
-    </div>
-  </>
-)
