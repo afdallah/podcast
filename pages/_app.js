5162787e4710f46f348864f70aaafbc3e3b68f0f
afdallah <afdallah.war@gmail.com>
2019-11-08 20:54:24 +0700

    Turn on coming soon page

--- pages/_app.js
+++ pages/_app.js
@@ -40,7 +40,7 @@ class MyApp extends App {
 
   render() {
     const { Component, pageProps, store } = this.props
-    const comingSoon = false
+    const comingSoon = true
 
     const props = {
       ...pageProps,
